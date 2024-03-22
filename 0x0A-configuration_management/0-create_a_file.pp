d69babc239f5cd6aea964ca9261a5aee895e3100
Tycee02 <wakilaminat@gmail.com>
2024-03-22 23:03:40 +0100

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,5 @@
-# creates a file in the /tmp directory
-file { '/tmp/holberton':
-  ensure  => 'file',
+# creates a file in the /tmp/school
+file { '/tmp/school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
