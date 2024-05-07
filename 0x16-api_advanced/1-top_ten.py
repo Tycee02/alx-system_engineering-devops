224f93e6a25a6fcf375c6249207b27821ecb70af
Tycee02 <wakilaminat@gmail.com>
2024-05-07 21:33:06 +0100

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-# function that queries the Reddit API
-# prints the titles of the first 10 hot posts listed for a given subreddit
-# If not a valid subreddit, print None
-
+""" Function that queries the Reddit API """
 import requests
+import sys
 
 
 def top_ten(subreddit):
-    # Reddit API URL for getting hot posts in a subreddit
+    """ Returns: top ten post titles
+        or None if queried subreddit is invalid """
+    headers = {'User-Agent': 'xica369'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'limit': 10}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
 
-    # Set custom User-Agent to avoid errors of Too Many Requests
-    headers = {'User-Agent': 'Custom User Agent'}
-
-    # Make a GET request to the Reddit API
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the request was successful
     if response.status_code == 200:
-        # Extract the titles of the first 10 hot posts
-        data = response.json()
-        posts = data['data']['children']
-        for i in range(10):
-            print("{}. {}".format(i + 1, posts[i]['data']['title']))
-    elif response.status_code == 404:
-        # If not a valid subreddit
-        print("None")
+        titles_ = response.json().get('data').get('children')
+        for title_ in titles_:
+            print(title_.get('data').get('title'))
     else:
-        # Other errors
-        print("Error occurred while fetching subreddit data.")
+        print(None)
